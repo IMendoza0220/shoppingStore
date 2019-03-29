95e8a86fe79e91beec22e658dbb6960ce8450d62
sally <sallyakrum83@gmail.com>
2019-03-29 17:34:55 -0500

    added some jquery

--- public/add_products.js
+++ public/add_products.js
@@ -1,15 +1,15 @@
 $(function () {
-    let productName = $('#productName')
-    let productManufacturer = $('#productManufacturer')
-    let productPrice = $('#productPrice')
+    let productName = $('#productName');
+    let productDepartment = $('#productDepartment');
+    let productPrice = $('#productPrice');
     $('#btnProductAdd').click(function () {
         addProduct(
             productName.val(),
-            productManufacturer.val(),
+            productDepartment.val(),
             productPrice.val(),
             function (addedProduct) {
                 window.alert("Added " + addedProduct.name + "to Database")
             }
         )
     })
-})
+});
