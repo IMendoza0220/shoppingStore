f8e68af9b4fc339b793c6e6997437403ab033a8f
Taylor F Stiger <taylorfgomez@gmail.com>
2019-03-31 22:29:36 -0500

    added cart button to index

--- public/customerCart.js
+++ public/customerCart.js
@@ -6,3 +6,8 @@ const
 
 
 })();
+
+
+<form class="form-inline my-2 my-lg-0">
+<button class="btn btn-outline-success btn-lg" type="button" id="cartButton">Cart <i class="fa fa-shopping-cart"></i></button>
+</form>
