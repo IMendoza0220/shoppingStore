da5fd05ef90cfe6d7fcfaa9a2374c8064cab5804
Adrian Morales <adrianmorales80@gmail.com>
2019-04-02 18:18:04 -0500

    updated cart

--- public/indexProdFunct.js
+++ public/indexProdFunct.js
@@ -22,14 +22,7 @@ function addProduct(name, department, price, picture, callback) {
 function addToCart(product) {
     
     $.get(`/api/products/${product.id}`, function(data) {
-        // var receiveddata = JSON.stringify(data);
-        // cartItems.push(receiveddata);
-        // localStorage.setItem('cartItems', cartItems.join(', '));
-        // Parse the serialized data back into an aray of objects
-        // Push the new data (whether it be an object or anything else) onto the array
         cartItems.push(data);
-        // Alert the array value
-        // Re-serialize the array back into a string and store it in localStorage
         localStorage.setItem('cartItems', JSON.stringify(cartItems));
     });
 }
